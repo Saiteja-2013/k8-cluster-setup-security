838541cc4536b71e9289e53b5d394d8cf4468112
Saiteja <93371297+Saiteja-2013@users.noreply.github.com>
2025-03-14 00:40:41 +0530

    Update variables.tf

--- terraform/eks/variables.tf
+++ terraform/eks/variables.tf
@@ -1,3 +1,40 @@
-variable "aws_region" { default = "us-west-2" }
-variable "cluster_name" { default = "secure-eks-cluster" }
-variable "subnet_ids" { type = list(string) }
+variable "github_owner" {
+  description = "GitHub organization or username"
+  type        = string
+  default     = "your-github-owner"
+}
+
+variable "github_repo" {
+  description = "GitHub repository name"
+  type        = string
+  default     = "your-repo-name"
+}
+
+variable "github_oauth_token" {
+  description = "GitHub OAuth Token for CodePipeline access"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_region" {
+  description = "AWS region where resources will be created"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "eks_cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "secure-eks-cluster"
+}
+
+variable "s3_bucket_name" {
+  description = "S3 bucket name for CodePipeline artifacts"
+  type        = string
+  default     = "eks-codepipeline-artifacts"
+}
+
+variable "subnet_ids" {
+  description = "List of subnet IDs for EKS cluster"
+  type        = list(string)
+}
